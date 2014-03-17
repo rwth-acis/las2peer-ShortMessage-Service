5b9789f19b6fdccc8ce220ea7edda7ed4f8fd16c
tcuje <tcuje@freenet.de>
2014-03-17 12:14:31 +0100

    project administration

---
+++ bin/launchNode.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# this script launches a LAS2peer node providing the ShortMessageService
+
+cd ..
+
+java -cp "lib/*" i5.las2peer.testing.L2pNodeLauncher -s 9010 - uploadStartupDirectory startService\(\'i5.las2peer.services.shortMessageService.ShortMessageService\',\'shortMessageServicePass\'\) startHttpConnector\(\'9080\'\) interactive
--- scripts/launch.sh
+++
@@ -1,5 +0,0 @@
-#cd ..
-BASE=.
-CLASSPATH=$BASE/lib/*;
-
-java -cp "$CLASSPATH" i5.las2peer.testing.L2pNodeLauncher windows_shell -s 9010 - uploadStartupDirectory startService\(\'i5.las2peer.services.shortMessageService.ShortMessageService\',\'shortMessageServicePass\'\) startHttpConnector\(\'9080\'\) interactive
