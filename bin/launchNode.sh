3e2fb20490e9b3bf788289c6b9ee0bc900d5c1e0
tcuje <tcuje@freenet.de>
2014-04-22 22:41:48 +0200

    minor fixes

--- bin/launchNode.sh
+++ bin/launchNode.sh
@@ -4,4 +4,4 @@
 
 cd ..
 
-java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -s 9010 - uploadStartupDirectory startService\(\'i5.las2peer.services.shortMessageService.ShortMessageService\',\'shortMessageServicePass\'\) startHttpConnector\(\'9080\'\) interactive
+java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -s 9010 - uploadStartupDirectory\(\'config/startup\'\) startService\(\'i5.las2peer.services.shortMessageService.ShortMessageService\',\'shortMessageServicePass\'\) startHttpConnector\(\'9080\'\) interactive
