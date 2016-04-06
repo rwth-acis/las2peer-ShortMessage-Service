8136e51c5ef92417e1cba868a865ca452c1b8e0b
Thomas Cujé <tcuje@freenet.de>
2016-04-06 17:04:17 +0200

    update startscript versioned services

--- bin/launchNode.sh
+++ bin/launchNode.sh
@@ -3,4 +3,4 @@
 # this script starts a LAS2peer node providing the ShortMessageService
 # pls run the script from the root folder of your deployment, e. g. ./bin/launchNode.sh
 
-java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -p 9010 uploadStartupDirectory\(\'etc/startup\'\) startService\(\'i5.las2peer.services.shortMessageService.ShortMessageService\',\'shortMessageServicePass\'\) startWebConnector interactive
+java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -p 9010 uploadStartupDirectory\(\'etc/startup\'\) startService\(\'i5.las2peer.services.shortMessageService.ShortMessageService@1.0\'\) startWebConnector interactive
